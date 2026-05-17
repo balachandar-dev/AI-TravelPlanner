11b53073f59cf63b183bc90dee0db70f2b43f9da
Balachandar <balachandar.mutharasu@gmail.com>
2026-05-17 12:33:48 +0530

    Initial Commit

---
+++ AI-TravelPlanner/AI_TravelPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_TravelPlannerApp.swift
+//  AI-TravelPlanner
+//
+//  Created by Balachandar on 17/05/26.
+//
+
+import SwiftUI
+
+@main
+struct AI_TravelPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI-TravelPlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AI-TravelPlanner
+//
+//  Created by Balachandar on 17/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
